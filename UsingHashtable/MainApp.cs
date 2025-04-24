c9794d0169ed453a6fc5ddc4886a2ce4bd79f69d
RND-HW3 <RND-HW3@RND-HW3>
2025-04-24 09:47:04 +0900

    초기 커밋.

---
+++ UsingHashtable/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace UsingHashtable
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["하나"] = "one";
+            ht["둘"] = "two";
+            ht[3] = "three";
+            ht[4.1] = "4.1";
+
+            WriteLine(ht["하나"]);
+            WriteLine(ht[3]);
+            WriteLine(ht[4.1]);
+        }
+    }
+}
